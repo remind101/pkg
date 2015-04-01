6ecc08ff271390ca292b7fad69c7aaab17b83282
Eric J. Holmes <eric@ejholmes.net>
2015-04-01 16:36:34 -0700

    Add an example.

--- example/main.go
+++ example/main.go
@@ -1,7 +1,68 @@
 package main
 
-import "net/http"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/remind101/pkg/httpx"
+	"github.com/remind101/pkg/httpx/middleware"
+	"github.com/remind101/pkg/reporter"
+	"golang.org/x/net/context"
+)
+
+func ok(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	io.WriteString(w, "Ok\n")
+	return nil
+}
+
+func bad(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	return &Error{ID: "bad_error", Err: errors.New("bad request")}
+}
+
+func boom(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	panic("boom")
+}
+
+func errorHandler(err error, w http.ResponseWriter, r *http.Request) {
+	http.Error(w, err.Error(), http.StatusInternalServerError)
+}
 
 func main() {
-	http.ListenAndServe(":8080")
+	// An error reporter that will log errors to stdout.
+	r := reporter.NewLogReporter()
+
+	m := httpx.NewRouter()
+
+	m.Handle("GET", "/ok", httpx.HandlerFunc(ok))
+	m.Handle("GET", "/bad", httpx.HandlerFunc(bad))
+	m.Handle("GET", "/boom", httpx.HandlerFunc(boom))
+
+	var h httpx.Handler
+
+	// Recover from panics, and report the recovered error to the reporter.
+	h = middleware.Recover(m, r)
+
+	// Handles any errors returned from handlers in a common format.
+	h = middleware.HandleError(h, errorHandler)
+
+	// Adds a logger to the context.Context that will log to stdout,
+	// prefixed with the request id.
+	h = middleware.NewLogger(h, os.Stdout)
+
+	// Adds the request id to the context.
+	h = middleware.ExtractRequestID(h)
+
+	http.ListenAndServe(":8080", middleware.BackgroundContext(h))
+}
+
+type Error struct {
+	ID  string
+	Err error
+}
+
+func (e *Error) Error() string {
+	return fmt.Sprintf("%s: %s", e.ID, e.Err)
 }
