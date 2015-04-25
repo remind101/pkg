2fde696b8ac74f083cedf36ecb394f0afa618f6e
Eric J. Holmes <eric@ejholmes.net>
2015-04-24 17:20:21 -0700

    Remove uneeded stuff.

--- httpx/middleware/response_writer.go
+++ httpx/middleware/response_writer.go
@@ -14,21 +14,16 @@ type ResponseWriter interface {
 	http.Flusher
 	// Status returns the status code of the response or 0 if the response has not been written.
 	Status() int
-	// Written returns whether or not the ResponseWriter has been written.
-	Written() bool
-	// Size returns the size of the response body.
-	Size() int
 }
 
 // NewResponseWriter creates a ResponseWriter that wraps an http.ResponseWriter
 func NewResponseWriter(rw http.ResponseWriter) ResponseWriter {
-	return &responseWriter{rw, 0, 0}
+	return &responseWriter{rw, 0}
 }
 
 type responseWriter struct {
 	http.ResponseWriter
 	status int
-	size   int
 }
 
 func (rw *responseWriter) WriteHeader(s int) {
@@ -36,26 +31,8 @@ func (rw *responseWriter) WriteHeader(s int) {
 	rw.ResponseWriter.WriteHeader(s)
 }
 
-func (rw *responseWriter) Write(b []byte) (int, error) {
-	if !rw.Written() {
-		// The status will be StatusOK if WriteHeader has not been called yet
-		rw.WriteHeader(http.StatusOK)
-	}
-	size, err := rw.ResponseWriter.Write(b)
-	rw.size += size
-	return size, err
-}
-
 func (rw *responseWriter) Status() int {
 	return rw.status
-}
-
-func (rw *responseWriter) Size() int {
-	return rw.size
-}
-
-func (rw *responseWriter) Written() bool {
-	return rw.status != 0
 }
 
 func (rw *responseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
