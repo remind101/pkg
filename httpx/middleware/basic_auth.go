e8a6c15bdf0d4d121e42e2ea81d0334d4379aaea
Tony Zou <tony@tonyzou.com>
2015-06-17 07:39:47 +1000

    added basic auth

---
+++ httpx/middleware/basic_auth.go
@@ -0,0 +1,70 @@
+package middleware
+
+import (
+	"encoding/base64"
+	"net/http"
+	"strings"
+	"fmt"
+
+	"github.com/remind101/pkg/httpx"
+	"golang.org/x/net/context"
+)
+
+type BasicAuther struct {
+	User, Pass string
+	Realm string
+
+	// The handler that will be called if the request is authorized.
+	Handler httpx.Handler
+
+	// The handler that will be called if the request is not authorized. The
+	// zero value is DefaultUnauthorizedHandler
+	UnauthorizedHandler httpx.Handler
+}
+
+func (a *BasicAuther) authenticated(r *http.Request) bool {
+	s := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
+	if len(s) != 2 || s[0] != "Basic" {
+		return false
+	}
+
+	b, err := base64.StdEncoding.DecodeString(s[1])
+	if err != nil {
+		return false
+	}
+
+	pair := strings.SplitN(string(b), ":", 2)
+	if len(pair) != 2 {
+		return false
+	}
+	return a.User == pair[0] && a.Pass == pair[1]
+}
+
+func DefaultUnauthorizedHandler(realm string) httpx.HandlerFunc {
+	return httpx.HandlerFunc(func (ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+		w.Header().Set("WWW-Authenticate", fmt.Sprintf(`Basic realm=%q`, realm))
+		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		return nil
+	})
+}
+
+func (a *BasicAuther) ServeHTTPContext(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	if a.authenticated(r) {
+		return a.Handler.ServeHTTPContext(ctx, w, r)
+	} else {
+		u := a.UnauthorizedHandler
+		if u == nil {
+			u = DefaultUnauthorizedHandler(a.Realm)
+		}
+		return u.ServeHTTPContext(ctx, w, r)
+	}
+}
+
+func BasicAuth(h httpx.Handler, user, pass, realm string) *BasicAuther {
+	return &BasicAuther{
+		User:    user,
+		Pass:    pass,
+		Realm:   realm,
+		Handler: h,
+	}
+}
---
+++ httpx/middleware/basic_auth_test.go
@@ -0,0 +1,59 @@
+package middleware
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/remind101/pkg/httpx"
+	"golang.org/x/net/context"
+)
+
+func TestBasicAuth(t *testing.T) {
+	ctx := context.Background()
+	resp := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/city/123.123.123.123", nil)
+	req.SetBasicAuth("user", "pass")
+
+	var h httpx.Handler
+
+	const test_str = "sfaftfofsfhfi"
+
+	h = httpx.HandlerFunc(func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+		w.Write([]byte(test_str))
+		return nil
+	})
+
+	h = BasicAuth(h, "user", "pass", "realm")
+	if err := h.ServeHTTPContext(ctx, resp, req); err != nil {
+		t.Fatal(err)
+	}
+
+	if got, want := resp.Body.String(), test_str; got != want {
+		t.Fatalf("Body => %s; want %s", got, want)
+	}
+}
+
+func TestBasicAuthUnauthorized(t *testing.T) {
+	ctx := context.Background()
+	resp := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/city/123.123.123.123", nil)
+
+	var h httpx.Handler
+
+	const test_str = "sfaftfofsfhfi"
+
+	h = httpx.HandlerFunc(func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+		w.Write([]byte(test_str))
+		return nil
+	})
+
+	h = BasicAuth(h, "user", "pass", "realm")
+	if err := h.ServeHTTPContext(ctx, resp, req); err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.Code != 401 {
+		t.Fatalf("Expected code 401")
+	}
+}
