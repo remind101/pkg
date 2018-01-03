a74fdc8665c9ae0dbddd099ac1123d7edd282ee8
Peter Hamilton <peterh@remind101.com>
2018-01-03 15:16:33 -0800

    remove dupe def

--- httpx/middleware/request_id.go
+++ httpx/middleware/request_id.go
@@ -41,18 +41,3 @@ func (h *RequestID) ServeHTTPContext(ctx context.Context, w http.ResponseWriter,
 	ctx = httpx.WithRequestID(ctx, requestID)
 	return h.handler.ServeHTTPContext(ctx, w, r)
 }
-
-// HeaderExtractor returns a function that can extract a request id from a list
-// of headers.
-func HeaderExtractor(headers []string) func(*http.Request) string {
-	return func(r *http.Request) string {
-		for _, h := range headers {
-			v := r.Header.Get(h)
-			if v != "" {
-				return v
-			}
-		}
-
-		return ""
-	}
-}
