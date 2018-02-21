8ce6fba5cd2f8620428f97c66482019a8ed0200d
Peter Hamilton <peterh@remind101.com>
2018-02-20 22:23:40 -0800

    scope header context keys

--- httpx/header.go
+++ httpx/header.go
@@ -2,11 +2,17 @@ package httpx
 
 import "context"
 
-func WithHeader(ctx context.Context, headerKey, headerValue string) context.Context {
-	return context.WithValue(ctx, headerKey, headerValue)
+type headerContextKey string
+
+func (c headerContextKey) String() string {
+	return "pkg header " + string(c)
+}
+
+func WithHeader(ctx context.Context, headerKey string, headerValue string) context.Context {
+	return context.WithValue(ctx, headerContextKey(headerKey).String(), headerValue)
 }
 
 func Header(ctx context.Context, headerKey string) string {
-	headerValue, _ := ctx.Value(headerKey).(string)
+	headerValue, _ := ctx.Value(headerContextKey(headerKey).String()).(string)
 	return headerValue
 }
